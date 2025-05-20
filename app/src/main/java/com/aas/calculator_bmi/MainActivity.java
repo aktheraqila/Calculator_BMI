e8984b1ae502f8a71dfba098e8006b6d02b51618
aktheraqila <aktheraqila@gmail.com>
2025-05-21 03:51:09 +0600

    Fixed editText

--- app/src/main/java/com/aas/calculator_bmi/MainActivity.java
+++ app/src/main/java/com/aas/calculator_bmi/MainActivity.java
@@ -16,31 +16,52 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Class Variables are also called 'fields'
+    private TextView resultText;
+    private Button calculateButton;
+    private RadioButton radioButtonMale;
+    private RadioButton radioButtonFemale;
+    private EditText editTextHeightFeet;
+    private EditText editTextHeightInches;
+    private EditText editTextWeight;
+    private EditText editTextAge;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        findViews();
+        setupButtonListener();
+    }
+    
+    private void findViews(){
+        resultText = findViewById(R.id.text_view_result);
+        radioButtonMale = findViewById(R.id.radio_button_male);
+        radioButtonFemale = findViewById(R.id.radio_button_female);
+        editTextHeightFeet = findViewById(R.id.edit_text_height_feet);
+        editTextHeightInches = findViewById(R.id.edit_text_height_inches);
+        editTextWeight = findViewById(R.id.edit_text_weight);
+        editTextAge = findViewById(R.id.edit_text_age);
+        calculateButton = findViewById(R.id.button_calculate);
 
-
-        TextView resultText = findViewById(R.id.text_view_result);
-
-        RadioButton radioButtonMale = findViewById(R.id.radio_button_male);
-        RadioButton radioButtonFemale = findViewById(R.id.radio_button_female);
-
-        EditText editTextHeightFeet = findViewById(R.id.edit_text_height_feet);
-        EditText editTextHeightInches = findViewById(R.id.edit_text_height_inches);
-        EditText editTextWeight = findViewById(R.id.edit_text_weight);
-        EditText editTextAge = findViewById(R.id.edit_text_age);
-
-        Button calculateButton = findViewById(R.id.button_calculate);
-
+    }
+    private void setupButtonListener() {
         calculateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,"Calculating BMI ...", Toast.LENGTH_LONG).show();
-
+                calculateBmi();
             }
         });
     }
+
+    private void calculateBmi() {
+        String TextHeightFeet = editTextHeightFeet.getText().toString();
+        String TextHeightInches = editTextHeightInches.getText().toString();
+        String TextWeight = editTextWeight.getText().toString();
+        String TextAge = editTextAge.getText().toString();
+
+        resultText.setText("Age: "+TextAge + " | Feet: " + TextHeightFeet + " | Inches: " + TextHeightInches+" | Weight: " + TextWeight);
+    }
+
 }
